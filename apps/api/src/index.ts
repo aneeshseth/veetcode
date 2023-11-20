b9d5e7e7afcc5c82c2c4c2d657a14e64e691eaa8
aneeshseth <aneeshseth2018@gmail.com>
2023-11-20 12:24:40 -0800

    added result checking

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,6 +4,8 @@ const app = express();
 const docker = new Docker({ socketPath: "/var/run/docker.sock" });
 const id = "2b552705876b";
 import cors from "cors";
+import util from "util";
+const setTimeoutPromise = util.promisify(setTimeout);
 
 app.use(cors());
 const problems = [
@@ -70,17 +72,33 @@ app.get("/problem", async (req, res) => {
 });
 
 app.post("/", async (req, res) => {
-  const { code } = req.body;
-  /*
+  //const { code } = req.body;
   let code = `
-  function sumArray() {
-    console.log("hello world") //hello world
-  }
-  sumArray()`;
-*/
+  function twoSum(nums, target) {
+    // Create a map to store the indices of elements we have seen so far
+    const numMap = new Map();
+
+    // Iterate through the array
+    for (let i = 0; i < nums.length; i++) {
+        // Calculate the complement (the value we need to reach the target)
+        const complement = target - nums[i];
+
+        // Check if the complement exists in the map
+        if (numMap.has(complement)) {
+            // Return the indices of the two numbers
+            return [numMap.get(complement), i];
+        }
+
+        // If the complement doesn't exist, store the current number and its index in the map
+        numMap.set(nums[i], i);
+    }
+
+    // If no solution is found
+    return null;
+}`;
+  let testPass = true;
   docker.getContainer(id).start(async (err, data) => {
     if (err) throw err;
-    console.log(data);
     try {
       const exec = await docker.getContainer(id).exec({
         Cmd: ["sh", "-c", `echo "${code}" > /data/foo`],
@@ -100,25 +118,51 @@ app.post("/", async (req, res) => {
             console.log("Second command done");
             stream?.on("data", async (chunk) => {
               output = chunk.toString("utf8");
-              console.log("output ", output);
-              console.log("code ", code);
             });
             stream?.on("end", async () => {
               let newCode = code.replace(/g\//g, "");
-              const exec3 = await docker.getContainer(id).exec({
-                Cmd: ["node", "-e", `${newCode}`],
-                AttachStdout: true,
-                AttachStderr: true,
-              });
-              const startExec3 = await exec3.start({}, async (err, stream) => {
-                console.log("third command done");
-                stream?.on("data", async (chunk) => {
-                  const output = chunk.toString("utf8");
-                  console.log("ouput for code ", output);
-                });
-              });
+              const testcasePromises = problems[0].testcases.map(
+                async (testcase) => {
+                  console.log("testcase ", testcase);
+                  const updatedCode =
+                    code + "\n" + `console.log(${testcase.functionCall})`;
+                  const exec3 = await docker.getContainer(id).exec({
+                    Cmd: ["node", "-e", `${updatedCode}`],
+                    AttachStdout: true,
+                    AttachStderr: true,
+                  });
+                  const startExec3 = await exec3.start(
+                    {},
+                    async (err, stream) => {
+                      console.log("third command done");
+                      stream?.on("data", async (chunk) => {
+                        console.log("DATA");
+                        const output = chunk.toString("utf8");
+                        const executionResult = output
+                          .replace(/\x1B\[[0-9;]*[mG]/g, "")
+                          .replace(/[\r\n]/g, "")
+                          .replace(/[^\w]/g, "");
+                        console.log("execresult", executionResult);
+                        const testresult = testcase.Output.replace(
+                          /\x1B\[[0-9;]*[mG]/g,
+                          ""
+                        )
+                          .replace(/[\r\n]/g, "")
+                          .replace(/[^\w]/g, "");
+                        console.log("test result", testresult);
+
+                        if (testresult != executionResult) {
+                          testPass = false;
+                        }
+                      });
+                    }
+                  );
+                }
+              );
+              await Promise.all(testcasePromises);
             });
           });
+          res.status(200).json({ msg: testPass });
         }, 1000);
       });
     } catch (err) {
